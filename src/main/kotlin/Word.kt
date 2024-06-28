5a2d92a07e48ca5942386767d38b4a3ab3c54af9
Maxim <55555lab@mail.ru>
2024-06-28 16:14:37 +0300

    ETB-04 fix

--- src/main/kotlin/Word.kt
+++ src/main/kotlin/Word.kt
@@ -14,18 +14,12 @@ fun String.toWord(): Word? {
     val parts = this.split("|")
     if (parts.size < AMOUNT_PARTS_IN_LINE) return null
 
-    return try {
-        val word = parts[0]
-        val translation = parts[1]
-        val correctAnswersCount = parts[2].toIntOrNull() ?: 0
-        Word(word, translation, correctAnswersCount)
-    } catch (e: NumberFormatException) {
-        null
-    }
+    val word = parts[0]
+    val translation = parts[1]
+    val correctAnswersCount = parts[2].toIntOrNull() ?: 0
+    return Word(word, translation, correctAnswersCount)
 }
 
-fun readWordsFromFile(fileName: String): MutableList<Word> {
-    val dictionary = mutableListOf<Word>()
-    File(fileName).readLines().forEach { it.toWord()?.let { line -> dictionary.add(line) } }
-    return dictionary
+fun readWordsFromFile(fileName: String): List<Word> {
+    return File(fileName).readLines().mapNotNull { it.toWord() }
 }
