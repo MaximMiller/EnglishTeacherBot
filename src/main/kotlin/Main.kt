dc2abf7fd59c8c2e18867e5d59a989b4c8b68799
Maxim <55555lab@mail.ru>
2024-07-02 20:29:02 +0300

    ETB-06 complete

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,21 @@
 package org.example
 
 fun main() {
+    fun List<Word>.getLearnedWordsCount() = count { it.correctAnswersCount >= 3 }
+
+    fun List<Word>.getLearnedWordsPercentage() = if (isNotEmpty()) {
+        getLearnedWordsCount() * 100 / size
+    } else {
+        0
+    }
+
+    fun printStatistics(words: List<Word>) {
+        val learnedWordsCount = words.getLearnedWordsCount()
+        val totalWordsCount = words.size
+        val learnedPercentage = words.getLearnedWordsPercentage()
+
+        println("Выучено $learnedWordsCount из $totalWordsCount слов | $learnedPercentage%")
+    }
     while (true) {
         println(
             """
@@ -14,7 +29,11 @@ fun main() {
         val answer = readln().toIntOrNull()
         when (answer) {
             1 -> println("Вы нажали на 1")
-            2 -> println("Вы нажали на 2")
+            2 -> {
+                val words = readWordsFromFile("words.txt")
+                printStatistics(words)
+            }
+
             0 -> break
             else -> println("Ошибка! Введите число только из меню")
         }
