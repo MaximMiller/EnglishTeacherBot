012387a209d508493ce948ea02947ddde7bb7e7c
Maxim <55555lab@mail.ru>
2024-08-09 16:24:38 +0300

    ETB-12 fix

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -5,11 +5,12 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
+const val BASE_URL = "https://api.telegram.org/bot"
 
 fun main(argument: Array<String>) {
     val botToken = argument[0]
-    val urlGetMe = "https://api.telegram.org/bot$botToken/getMe"
-    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates"
+    val urlGetMe = "$BASE_URL$botToken/getMe"
+    val urlGetUpdates = "$BASE_URL$botToken/getUpdates"
 
     val client = HttpClient.newBuilder().build()
     val request = HttpRequest.newBuilder().uri(URI.create(urlGetMe)).build()
