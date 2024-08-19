173de17b2fdcba1c4f2547905d79122e0ab5a4ed
Maxim <55555lab@mail.ru>
2024-08-19 09:29:03 +0300

    ETB-15 complete

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,36 +1,28 @@
 package org.example
 
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
-
 const val BASE_URL = "https://api.telegram.org/bot"
 
 fun main(argument: Array<String>) {
     val botToken = argument[0]
     var nextUpdateId = 0
+    var chatId = 0
+    val telegramBotService = TelegramBotService()
 
     while (true) {
         Thread.sleep(1000)
-        val updates = getUpdates(botToken, nextUpdateId)
+        val updates = telegramBotService.getUpdates(botToken, nextUpdateId)
         val updateIdRegex = Regex(pattern = "\"update_id\":\\s*(\\d+)")
-        val matches = updateIdRegex.find(updates)
+        val chatIdRegex = Regex(pattern = "\"chat\":\\{\"id\":\\s*(\\d+)")
+        val matchesUpdateId = updateIdRegex.find(updates)
+        val matchesChatId = chatIdRegex.find(updates)
         println(updates)
-        if (matches != null) {
-            val updateId = matches.groupValues[1].toInt()
+        if (matchesUpdateId != null) {
+            val updateId = matchesUpdateId.groupValues[1].toInt()
             nextUpdateId = updateId + 1
-            println(updates)
+            if (matchesChatId != null) {
+                chatId = matchesChatId.groupValues[1].toInt()
+            }
+            telegramBotService.sendMessage(botToken,chatId,"Hello")
         }
     }
 }
-
-fun getUpdates(botToken: String, updateId: Int): String {
-    val urlWithOffset = "$BASE_URL$botToken/getUpdates?offset=$updateId"
-    val client = HttpClient.newBuilder().build()
-    val requestGetUpdate = HttpRequest.newBuilder().uri(URI.create(urlWithOffset)).build()
-    val responseGetUpdates = client.send(requestGetUpdate, HttpResponse.BodyHandlers.ofString())
-    return responseGetUpdates.body()
-}
-
-
