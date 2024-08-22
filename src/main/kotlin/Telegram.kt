1c508e7e3b7d2868d1716d540ed30737fc43709d
Maxim <55555lab@mail.ru>
2024-08-22 21:48:25 +0300

    ETB-16 need check

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,28 +1,50 @@
 package org.example
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
 const val BASE_URL = "https://api.telegram.org/bot"
 
 fun main(argument: Array<String>) {
     val botToken = argument[0]
-    var nextUpdateId = 0
-    var chatId = 0
-    val telegramBotService = TelegramBotService()
+    var lastUpdateId = 0
+    val telegramBotService = TelegramBotService(botToken)
+    val json = Json { ignoreUnknownKeys = true }
 
     while (true) {
         Thread.sleep(1000)
-        val updates = telegramBotService.getUpdates(botToken, nextUpdateId)
-        val updateIdRegex = Regex(pattern = "\"update_id\":\\s*(\\d+)")
-        val chatIdRegex = Regex(pattern = "\"chat\":\\{\"id\":\\s*(\\d+)")
-        val matchesUpdateId = updateIdRegex.find(updates)
-        val matchesChatId = chatIdRegex.find(updates)
-        println(updates)
-        if (matchesUpdateId != null) {
-            val updateId = matchesUpdateId.groupValues[1].toInt()
-            nextUpdateId = updateId + 1
-            if (matchesChatId != null) {
-                chatId = matchesChatId.groupValues[1].toInt()
+
+        val updatesJson: String? = telegramBotService.getUpdates(lastUpdateId)
+        println(updatesJson)
+
+        val updates = updatesJson?.let {
+            json.decodeFromString<UpdateResponse>(it)
+        } ?: continue
+
+        updates.result.forEach { update ->
+            val updateId = update.update_id
+            val chatId = update.message?.chat?.id
+            val message = update.message?.text
+            lastUpdateId = updateId + 1
+
+            if (message != null && chatId != null) {
+                when (message.lowercase()) {
+                    "hello" -> telegramBotService.sendMessage(chatId, "Hello")
+                    "start" -> telegramBotService.sendMenu(chatId)
+                }
             }
-            telegramBotService.sendMessage(botToken,chatId,"Hello")
         }
     }
 }
+
+@Serializable
+data class UpdateResponse(val ok: Boolean, val result: List<Update>)
+
+@Serializable
+data class Update(val update_id: Int, val message: Message?)
+
+@Serializable
+data class Message(val chat: Chat, val text: String?)
+
+@Serializable
+data class Chat(val id: Int)
--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -1,35 +1,118 @@
 package org.example
 
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okio.IOException
+import kotlinx.serialization.encodeToString
 
-class TelegramBotService {
-    fun getUpdates(botToken: String, updateId: Int): String {
+@Serializable
+data class SendMessageRequest(
+    val chat_id: Int,
+    val text: String,
+)
+
+@Serializable
+data class SendMenuRequest(
+    val chat_id: Int,
+    val text: String,
+    val reply_markup: InlineKeyboardMarkup,
+)
+
+@Serializable
+data class InlineKeyboardMarkup(
+    val inline_keyboard: List<List<InlineKeyboardButton>>,
+)
+
+@Serializable
+data class InlineKeyboardButton(
+    val text: String,
+    val callback_data: String,
+)
+
+class TelegramBotService(private val botToken: String) {
+    private val client = OkHttpClient()
+    fun getUpdates(updateId: Int): String? {
         val urlWithOffset = "$BASE_URL$botToken/getUpdates?offset=$updateId"
-        val client = HttpClient.newBuilder().build()
-        val requestGetUpdate = HttpRequest.newBuilder().uri(URI.create(urlWithOffset)).build()
-        val responseGetUpdates = client.send(requestGetUpdate, HttpResponse.BodyHandlers.ofString())
-        return responseGetUpdates.body()
+        val requestGetUpdate = Request.Builder()
+            .url(urlWithOffset)
+            .build()
+
+        return try {
+            client.newCall(requestGetUpdate).execute().use { responseGetUpdate ->
+                if (responseGetUpdate.isSuccessful) {
+                    responseGetUpdate.body?.string()
+                } else {
+                    val errorMsg = "Failed to get updates: ${responseGetUpdate.code} ${responseGetUpdate.message}"
+                    throw IOException(errorMsg)
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            null
+        }
     }
 
-    fun sendMessage(botToken: String, chatId: Int, message: String): String {
+    fun sendMessage(chatId: Int, message: String): String? {
         val urlWithSendMessage = "$BASE_URL$botToken/sendMessage"
-        val bodySendMessage = """
-            {
-                "chat_id": $chatId,
-                "text": "$message"
-            }
-        """.trimIndent()
+        val sendMessageRequest = SendMessageRequest(chatId, message)
+        val bodySendMessage = Json.encodeToString(sendMessageRequest)
+        val mediaType = "application/json; charset=utf-8".toMediaType()
+        val requestBody = bodySendMessage.toRequestBody(mediaType)
 
-        val client = HttpClient.newBuilder().build()
-        val requestSendMessage = HttpRequest.newBuilder()
-            .uri(URI.create(urlWithSendMessage))
-            .header("Content-Type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString(bodySendMessage))
+        val requestSendMessage = Request.Builder()
+            .url(urlWithSendMessage)
+            .post(requestBody)
             .build()
-        val responseSendMessage = client.send(requestSendMessage, HttpResponse.BodyHandlers.ofString())
-        return responseSendMessage.body()
+
+        return try {
+            client.newCall(requestSendMessage).execute().use { responseSendMessage ->
+                if (responseSendMessage.isSuccessful) {
+                    responseSendMessage.body?.string()
+                } else {
+                    val errorMsg = "Failed to send message: ${responseSendMessage.code} ${responseSendMessage.message}"
+                    throw IOException(errorMsg)
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            null
+        }
+
+    }
+
+    fun sendMenu(chatId: Int): String? {
+        val urlWithSendMenu = "$BASE_URL$botToken/sendMessage"
+        val inlineKeyboard = listOf(
+            listOf(
+                InlineKeyboardButton("Изучить слова", "learn_words_clicked"),
+                InlineKeyboardButton("Статистика", "statistics_clicked")
+            )
+        )
+        val sendMenuRequest = SendMenuRequest(chatId, "Основное меню", InlineKeyboardMarkup(inlineKeyboard))
+        val bodySendMenu = Json.encodeToString(sendMenuRequest)
+        val mediaType = "application/json; charset=utf-8".toMediaType()
+        val requestBody = bodySendMenu.toRequestBody(mediaType)
+        val requestSendMenu = Request.Builder()
+            .url(urlWithSendMenu)
+            .post(requestBody)
+            .build()
+
+        return try {
+            client.newCall(requestSendMenu).execute().use { responseSendMenu ->
+                if (responseSendMenu.isSuccessful) {
+                    responseSendMenu.body?.string()
+                } else {
+                    val errorMsg = "Failed to send menu: ${responseSendMenu.code} ${responseSendMenu.message}"
+                    throw IOException(errorMsg)
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            null
+        }
     }
 }
