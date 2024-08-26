4f810d8b1f7e357bac76a788b91e627a16b641b9
Maxim <55555lab@mail.ru>
2024-08-26 18:01:29 +0300

    ETB-16 complete

--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -65,52 +65,9 @@ class TelegramBotService(
             .header("Content-Type", "application/json; charset=UTF-8")
             .POST(HttpRequest.BodyPublishers.ofString(bodySendMessage))
             .build()
-        return try {
-            client.newCall(requestSendMessage).execute().use { responseSendMessage ->
-                if (responseSendMessage.isSuccessful) {
-                    responseSendMessage.body?.string()
-                } else {
-                    val errorMsg = "Failed to send message: ${responseSendMessage.code} ${responseSendMessage.message}"
-                    throw IOException(errorMsg)
-                }
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-            null
-        }
+        val responseSendMessage = client.send(requestSendMessage, HttpResponse.BodyHandlers.ofString())
+        return responseSendMessage.body()
 
-    }
-
-    fun sendMenu(chatId: Int): String? {
-        val urlWithSendMenu = "$BASE_URL$botToken/sendMessage"
-        val inlineKeyboard = listOf(
-            listOf(
-                InlineKeyboardButton("Изучить слова", "learn_words_clicked"),
-                InlineKeyboardButton("Статистика", "statistics_clicked")
-            )
-        )
-        val sendMenuRequest = SendMenuRequest(chatId, "Основное меню", InlineKeyboardMarkup(inlineKeyboard))
-        val bodySendMenu = Json.encodeToString(sendMenuRequest)
-        val mediaType = "application/json; charset=utf-8".toMediaType()
-        val requestBody = bodySendMenu.toRequestBody(mediaType)
-        val requestSendMenu = Request.Builder()
-            .url(urlWithSendMenu)
-            .post(requestBody)
-            .build()
-
-        return try {
-            client.newCall(requestSendMenu).execute().use { responseSendMenu ->
-                if (responseSendMenu.isSuccessful) {
-                    responseSendMenu.body?.string()
-                } else {
-                    val errorMsg = "Failed to send menu: ${responseSendMenu.code} ${responseSendMenu.message}"
-                    throw IOException(errorMsg)
-                }
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-            null
-        }
     }
 
     fun sendMenu(chatId: Int): String {
